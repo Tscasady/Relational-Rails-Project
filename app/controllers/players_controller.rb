680881fe1f8019fe4f8702726b1433858e1d52b4
Thomas Casady <tscasady@gmail.com>
2022-11-28 16:15:39 -0600

    feat: Add create method to PlayersController.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,4 +2,12 @@ class PlayersController < ApplicationController
   def index
     @players = Player.all
   end
+
+  def create
+    player = Player.new({
+      name: params[:player][:name]
+    })
+
+    player.save
+  end
 end
