659183721ece116eb517a3dde62c7d05858f247b
Thomas Casady <tscasady@gmail.com>
2022-11-28 16:38:57 -0600

    feat: Add games index and show routes.

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
 
   get '/players', to: 'players#index'
   get '/players/:id', to: 'players#show'
+
+  get '/games', to: 'games#index'
+  get '/games/:id', to: 'games#show'
 end
