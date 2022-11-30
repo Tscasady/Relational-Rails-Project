cbfb8a18507bd23a5b3170a0553f394b96021906
Thomas Casady <tscasady@gmail.com>
2022-11-29 18:35:41 -0600

    fix: unneeded create methods and dynamic games index.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,14 +2,6 @@ class GamesController < ApplicationController
   def index
     @games = Game.all
   end
-
-  def create
-    game = Game.new({
-      name: params[:game][:name]
-    })
-
-    game.save
-  end
   
   def show
     @game = Game.find(params[:id])
