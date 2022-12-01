f764227f04adf5af104493afa798279ffc961779
Thomas Casady <tscasady@gmail.com>
2022-12-01 17:05:59 -0600

    test: Add test for ordered_games method for Players.

--- spec/models/players_spec.rb
+++ spec/models/players_spec.rb
@@ -4,4 +4,15 @@ RSpec.describe Player do
   describe 'relationship' do
     it { should have_many :games}
   end
+
+  describe '#ordered_games' do
+    it 'orders all games by created_at in ascending order' do
+      player1 = Player.create!(name: 'Magnus', rating: 3000, age: 32)
+      game1 = player1.games.create!(name: 'Tournament1Round1', created_at: Time.now-2.days, won?: true, pieces: 'black')
+      game2 = player1.games.create!(name: 'Tournament1Round2', created_at: Time.now, won?: false, pieces: 'white')
+      game3 = player1.games.create!(name: 'Tournament1Round3', created_at: Time.now-4.days, won?: true, pieces: 'black')
+
+      expect(Player.ordered_games).to eq [game2, game3, game1]
+    end
+  end
 end
