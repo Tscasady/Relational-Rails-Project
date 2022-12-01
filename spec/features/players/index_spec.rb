0c93c9a5a8daf96dbcf25f72e2760b54543a49b6
Thomas Casady <tscasady@gmail.com>
2022-11-30 20:01:12 -0600

    test: Add /players/ feature test.

---
+++ spec/features/players/index_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'the players index page' do
+  it 'displays all players via their names' do
+    player1 = Player.create!(name: 'Magnus')
+    player2 = Player.create!(name: 'Anthony')
+
+    visit '/players'
+
+    save_and_open_page
+
+    expect(page).to have_content(player1.name)
+    expect(page).to have_content(player2.name)
+  end
+end
