f21ae82c9840ea44b484d154a0cec8c16ce10b01
Thomas Casady <tscasady@gmail.com>
2022-12-01 20:21:27 -0600

    test: Add test for model and view for games played by a player.

--- spec/features/players/show_spec.rb
+++ spec/features/players/show_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe 'the players show page' do
     expect(page).to have_content(player1.age)
     expect(page).to_not have_content(player2.name)
   end
+
+  it 'displays the number of games a player has played' do
+    player1 = Player.create!(name: 'Magnus', rating: 3000, age: 32)
+    player2 = Player.create!(name: 'Anthony', rating: 1100, age: 38)
+    game1 = player1.games.create!(name: 'Tournament1Round1', won?: true, pieces: 'black')
+    game2 = player1.games.create!(name: 'Tournament1Round2', won?: false, pieces: 'white')
+    game3 = player2.games.create!(name: 'Tournament1Round1', won?: false, pieces: 'black')
+
+    visit "/players/#{player1.id}"
+
+    expect(page).to have_content("Games Played: #{player1.games.count}")
+  end
 end
--- spec/models/players_spec.rb
+++ spec/models/players_spec.rb
@@ -13,4 +13,17 @@ RSpec.describe Player do
       expect(Player.ordered).to eq [player2, player3, player1]
     end
   end
+
+  describe '#game_count' do
+    it 'returns the number of games played by the player' do
+      player1 = Player.create!(name: 'Magnus', created_at: Time.now-4.days, rating: 3000, age: 32)
+      player2 = Player.create!(name: 'Anthony', created_at: Time.now, rating: 1100, age: 38)
+      game1 = player1.games.create!(name: 'Tournament1Round1', won?: true, pieces: 'black')
+      game2 = player1.games.create!(name: 'Tournament1Round2', won?: false, pieces: 'white')
+      game3 = player2.games.create!(name: 'Tournament1Round1', won?: false, pieces: 'black')
+
+      expect(player1.game_count).to eq 2
+      expect(player2.game_count).to eq 1
+    end
+  end
 end
