ded2f8a71011f0576be0a2b8c8d703b12842378b
Thomas Casady <tscasady@gmail.com>
2022-12-01 19:20:19 -0600

    feat: Add ordered method to Players.

--- spec/models/players_spec.rb
+++ spec/models/players_spec.rb
@@ -5,14 +5,12 @@ RSpec.describe Player do
     it { should have_many :games}
   end
 
-  describe '#ordered_games' do
+  describe '#ordered' do
     it 'orders all games by created_at in ascending order' do
-      player1 = Player.create!(name: 'Magnus', rating: 3000, age: 32)
-      game1 = player1.games.create!(name: 'Tournament1Round1', created_at: Time.now-2.days, won?: true, pieces: 'black')
-      game2 = player1.games.create!(name: 'Tournament1Round2', created_at: Time.now, won?: false, pieces: 'white')
-      game3 = player1.games.create!(name: 'Tournament1Round3', created_at: Time.now-4.days, won?: true, pieces: 'black')
-
-      expect(Player.ordered_games).to eq [game2, game3, game1]
+      player1 = Player.create!(name: 'Magnus', created_at: Time.now-4.days, rating: 3000, age: 32)
+      player2 = Player.create!(name: 'Anthony', created_at: Time.now, rating: 1100, age: 38)
+      player3 = Player.create!(name: 'Judit', created_at: Time.now-2.days, rating: 2700, age: 46)
+      expect(Player.ordered).to eq [player2, player3, player1]
     end
   end
 end
