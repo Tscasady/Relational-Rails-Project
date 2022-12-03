5082a033906846d7b515a3ae84fab0f144b6fcd4
Thomas Casady <tscasady@gmail.com>
2022-12-03 13:31:05 -0600

    feat: Add route for players/new

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   get '/', to: 'welcome#index'
 
   get '/players', to: 'players#index'
+  get '/players/new', to: 'players#new'
   get '/players/:id', to: 'players#show'
   get '/players/:id/games', to: 'players/games#index'
-
   get '/games', to: 'games#index'
   get '/games/:id', to: 'games#show'
 end
