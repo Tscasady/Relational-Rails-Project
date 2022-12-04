52165f36eb12757d9eadf19e217b167c61e88ce2
Thomas Casady <tscasady@gmail.com>
2022-12-03 18:34:57 -0600

    feat: Add patch route to update and get route to edit page.

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
   get '/games', to: 'games#index'
   get '/games/:id', to: 'games#show'
   post '/players', to: 'players#create'
+  get '/players/:id/edit', to: 'players#edit'
+  patch 'players/:id/', to: 'players#update'
 end
