69e4f3fc9f080ceef481bb0e23a4e3a793759605
Thomas Casady <tscasady@gmail.com>
2022-12-04 20:00:26 -0600

    feat: Add update and edit methods to games controller.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,4 +6,14 @@ class GamesController < ApplicationController
   def show
     @game = Game.find(params[:id])
   end
+
+  def edit
+    @game = Game.find(params[:id])
+  end
+  
+  def update
+    game = Game.find(params[:id])
+    game.update(name: params[:name], won?: params[:won], pieces: params[:pieces])
+    redirect_to "/games/#{game.id}"
+  end
 end
