33536ba9803698558b9fbfcb009aadf390f04c06
Thomas Casady <tscasady@gmail.com>
2022-12-04 18:51:03 -0600

    test: Add test for update games button on a game show page.

--- spec/features/games/show_spec.rb
+++ spec/features/games/show_spec.rb
@@ -13,4 +13,14 @@ RSpec.describe 'the games show page' do
     expect(page).to_not have_content(game2.name)
  
   end
+
+  it 'has an update game button' do
+    player1 = Player.create!(name: 'Magnus', rating: 3000, age: 32)
+    game1 = player1.games.create!(name: 'Tournament1Round1', won?: true, pieces: 'black')
+    game2 = player1.games.create!(name: 'Tournament1Round2', won?: false, pieces: 'white')
+
+    visit "/games/#{game1.id}"
+
+    expect(page).to have_link "Update Game", href: "/games/#{game1.id}/edit"
+  end
 end
