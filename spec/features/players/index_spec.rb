999cfdc5c85abf0101ef5877ccea6873036c5dbf
Thomas Casady <tscasady@gmail.com>
2022-12-05 19:46:56 -0600

    test: Add test for edit button on player index.

--- spec/features/players/index_spec.rb
+++ spec/features/players/index_spec.rb
@@ -26,4 +26,14 @@ RSpec.describe 'the players index page' do
     expect(page).to have_link "#{player1.name}", href: "/players/#{player1.id}"
     expect(page).to have_link "#{player2.name}", href: "/players/#{player2.id}"
   end
+
+  it 'has a link to edit each parent' do
+    player1 = Player.create!(name: 'Magnus', rating: 3000, professional: true)
+    player2 = Player.create!(name: 'Anthony', rating: 1100, professional: false)
+
+    visit '/players'
+
+    expect(page).to have_link "Edit #{player1.name}", href: "/players/#{player1.id}/edit"
+    expect(page).to have_link "Edit #{player2.name}", href: "/players/#{player2.id}/edit"
+  end
 end
