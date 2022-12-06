13de25b862173e0ea296379a3fac72f36d02ad8b
Thomas Casady <tscasady@gmail.com>
2022-12-05 18:13:21 -0600

    feat: Add ordered_games method to player and add to controller.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ player2 = Player.create(name: "Anthony", rating: 1100, professional: false)
 player3 = Player.create(name: "Judit", rating: 2700, professional: true)
 player4 = Player.create(name: "Kaylah Rose", rating: 1200, professional: false)
 player5 = Player.create(name: "Thomas", rating: 1300, professional: false)
-Game.create(name: "Sinquefield Cup Round 1", won: true, number_of_moves: rand(10..80), player: player1)Tournament1Round1
+Game.create(name: "Sinquefield Cup Round 1", won: true, number_of_moves: rand(10..80), player: player1)
 Game.create(name: "Grand Prix 2022", won: true, number_of_moves: rand(10..80), player: player1)
 Game.create(name: "Sinquefield Cup Round 1", won: false, number_of_moves: rand(10..80), player: player2)
 Game.create(name: "Grand Prix 2022", won: true, number_of_moves: rand(10..80), player: player2)
