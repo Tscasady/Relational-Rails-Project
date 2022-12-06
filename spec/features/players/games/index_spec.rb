d1ea702b62c982a062ef97e971d628889e7e21ac
Thomas Casady <tscasady@gmail.com>
2022-12-05 21:13:43 -0600

    feat: Add tests and links for game edit page to game index pages.

--- spec/features/players/games/index_spec.rb
+++ spec/features/players/games/index_spec.rb
@@ -39,9 +39,20 @@ RSpec.describe "the players' games index page" do
     game2 = player1.games.create(name: "Grand Prix 2022", won: true, number_of_moves: rand(10..80), player: player1)
 
     visit "/players/#{player1.id}/games"
-    save_and_open_page
     click_link 'Sort Alphabetically'
 
     expect(game2.name).to appear_before (game1.name) 
+  end
+
+  it 'has a link to edit each game' do
+    player1 = Player.create!(name: 'Magnus', rating: 3000, professional: true)
+    game1 = player1.games.create!(name: 'Tournament1Round1', won: true, number_of_moves: 24)
+    game2 = player1.games.create!(name: 'Sinquefield Cup Round 1', won: false, number_of_moves: 21)
+    game3 = player1.games.create!(name: 'Grand Prix round 1', won: true, number_of_moves: 29)
+
+    visit "/players/#{player1.id}/games"
+
+    expect(page).to have_link "Edit #{game1.name}", href: "/games/#{game1.id}/edit"
+    expect(page).to have_link "Edit #{game3.name}", href: "/games/#{game3.id}/edit"
   end
 end
