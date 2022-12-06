9c99515405a38826cc185027473dbd84275eb890
Thomas Casady <tscasady@gmail.com>
2022-12-06 16:54:42 -0600

    test: Add number_of_moves test to player games index

--- spec/features/players/games/index_spec.rb
+++ spec/features/players/games/index_spec.rb
@@ -55,4 +55,18 @@ RSpec.describe "the players' games index page" do
     expect(page).to have_link "Edit #{game1.name}", href: "/games/#{game1.id}/edit"
     expect(page).to have_link "Edit #{game3.name}", href: "/games/#{game3.id}/edit"
   end
+
+  it 'has a numerical field to filter games by number of moves' do
+    player1 = Player.create!(name: 'Magnus', rating: 3000, professional: true)
+    game1 = player1.games.create!(name: 'Tournament1Round1', won: true, number_of_moves: 24)
+    game2 = player1.games.create!(name: 'Sinquefield Cup Round 1', won: false, number_of_moves: 21)
+    game3 = player1.games.create!(name: 'Grand Prix round 1', won: true, number_of_moves: 29)
+
+    visit "/players/#{player1.id}/games"
+    fill_in "number_of_moves", with: 23
+
+    expect(page).to have_content game1.number_of_moves
+    expect(page).to_not have_content game2.number_of_moves
+    expect(page).to have_content game3.number_of_moves
+  end
 end
