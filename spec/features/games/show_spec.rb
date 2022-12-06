f952e32148de59bb323db3975832d1f6a758d2cf
Thomas Casady <tscasady@gmail.com>
2022-12-06 14:15:13 -0600

    test: Add test for game delete functionality.

--- spec/features/games/show_spec.rb
+++ spec/features/games/show_spec.rb
@@ -25,4 +25,18 @@ RSpec.describe 'the games show page' do
 
     expect(current_path).to eq("/games/#{game1.id}/edit")
   end
+  
+  it 'has a link that can delete a game' do
+    player1 = Player.create!(name: 'Magnus', rating: 3000, professional: true)
+    game1 = player1.games.create!(name: 'Tournament1Round1', won: true, number_of_moves: 43)
+    game2 = player1.games.create!(name: 'Tournament1Round1', won: true, number_of_moves: 31)
+    
+    visit "/players/#{player1.id}"
+    
+    click_button "Delete Player"
+
+    expect(current_path).to eq "/players"
+    expect(page).to_not have_content(game1.name)
+    expect(page).to have_content(game2.name)
+  end
 end
